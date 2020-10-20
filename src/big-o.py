67bc28449b6cba48bd4978fb92a887efe589979f
dforoni <deborah.foroni@gmail.com>
2020-10-20 08:46:48 -0300

    big-o conceitos

---
+++ src/big-o.py
@@ -0,0 +1,9 @@
+## Notação Big-O
+'''
+- Como comparar algoritmos?
+- Comparação objetiva entre algoritmos.
+- Considera diferenças entre poder de processamento, sistema operacional, linguagem de programação.
+- O quanto a "complexidade" do algoritmo aumenta de acordo com as entradas.
+
+'''
+
